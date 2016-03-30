2edc64902059768eb1a484d1f104f35603f75520
Songfuzhen <evil_song@126.com>
2016-03-30 13:47:17 +0800

    welcome page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyRoad::Application.config.secret_token = 'cb2e8458f442b61382cc0a6126f7b23218c2f39d165820070a30d435c24b2f97e28d4cfa14b8187f20092f4da2b0055c1e174d1f6ec808430020763f23f615f2'
